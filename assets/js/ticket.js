51ada9eac4e820340da27ddc8ce119ff2c786817
unknown <pp_nancy@hotmail.com>
2019-01-14 15:07:42 -0500

    Fited display 5 results in the loop command

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,49 +1,62 @@
-$("#run-search").on("click", function(event) {
-  // This line allows us to take advantage of the HTML "submit" property
-  // This way we can hit enter on the keyboard and it registers the search
-  // (in addition to clicks). Prevents the page from reloading on form submit.
+var data;
+
+$("#run-search").on("click", function (event) {
+
   event.preventDefault();
 
-  // Empty the region associated with the articles
-  clear();
-
-  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?&"+queryParams;
-  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?"+"keyword="+ keyword +"&countyCode="+ location+queryParams;
-
-  // Begin building an object to contain our API call's query parameters
   // Set the API key
-  var queryParams = "apikey=RAgvUGZ0z6k7b7KSCvNST1WATfPj064c" ;
+  var queryParams = "&apikey=RAgvUGZ0z6k7b7KSCvNST1WATfPj064c";
 
   // Grab text the user typed into the search input, add to the queryParams object
-  var location = $("#search-location")
-    .val()
-    .trim();
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-  var keyword = $("#search-keyword")
-    .val()
-    .trim();
+  var keyword = $("#search-keyword").val().trim();
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+  //
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + "keyword=" + keyword + queryParams;
 
-      // Create a new table row element
-      var tRow = $("<tr>");
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-      // Methods run on jQuery selectors return the selector they we run on
-      // This is why we can create and save a reference to a td in the same statement we update its text
-      var titleTd = $("<td>").text(response.Title);
-      var yearTd = $("<td>").text(response.Year);
-      var actorsTd = $("<td>").text(response.Actors);
-        
-      // Append the newly created table data to the table row
-      tRow.append(titleTd, yearTd, actorsTd);
-      // Append the table row to the table body
-      $("tbody").append(tRow);
-    });
+    console.log(response);
+    var data = response._embedded.events;
+
+    for (i = 0; i < 5; i++) {
+      var dataDiv = $("<div class='data'>");
+    
+    // name display
+     var name = data[i].name;
+     var pOne = $("<p>").text("Name of Ticket: " + name);
+     dataDiv.append(pOne);
+
+     // website
+   
+    var website = data[i].url;
+    var pTwo = $("<p>").text("Website: " + website);
+     dataDiv.append(pTwo);
+
+     //date
+    console.log(data[i].dates.start);
+     var date = data[i].dates.start.localDate;
+     var pThree = $("<p>").text("Date: " + date);
+     dataDiv.append(pThree); 
+
+     // Retrieving the URL for the image
+     var imgURL = data[i].images[0].url;
+     console.log(imgURL);
+
+     // Creating an element to hold the image
+     var image = $("<img class='img-fluid'>").attr("src", imgURL);
+
+     // Appending the image
+     dataDiv.append(image);
+
+     $("#events-section").prepend(dataDiv);
+
+    }
 
 
+  })
 
+
+})

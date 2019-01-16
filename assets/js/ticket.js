9169f66219f038d976cf84266156e84bd2323af1
ShaBree Quinones <shabreeq@gmail.com>
2019-01-16 00:05:57 -0500

    api's

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -12,7 +12,7 @@ $("#run-search").on("click", function (event) {
   var keyword = $("#search-keyword").val().trim();
 
   //
-  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + "keyword=" + keyword + queryParams;
+  var queryURL = "https://api.ticketmaster.com/discovery/v2/events.json?" + "keyword=" + keyword + queryParams;
 
   $.ajax({
     url: queryURL,

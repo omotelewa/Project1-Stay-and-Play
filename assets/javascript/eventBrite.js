8cd46eca7d4f3433a5f5e4254081c52bb861e9f7
unknown <pp_nancy@hotmail.com>
2019-05-12 20:46:00 -0400

    Fix format in Enterainment.js

--- assets/javascript/eventBrite.js
+++ assets/javascript/eventBrite.js
@@ -2,26 +2,26 @@ var eventData;
 var newEvent;
 $(document).ready(function () {
 
-    
-    $("#searchButton").on("click", function (event) {
-      event.preventDefault();
+
+  $("#searchButton").on("click", function (event) {
+    event.preventDefault();
     var searchTerm = $("#searchSite").val().trim();
-    
+
     console.log("searchTerm: ", searchTerm);
-    
-    var queryURL = 'https://www.eventbriteapi.com/v3/events/search?location.address='+ searchTerm +'&expand=venue&token=BAMDIYWNYH4MSFMA3WYF&expand=venue';
-    
+
+    var queryURL = 'https://www.eventbriteapi.com/v3/events/search?location.address=' + searchTerm + '&expand=venue&token=BAMDIYWNYH4MSFMA3WYF&expand=venue';
+
     var corsURL = "https://alex-rosencors.herokuapp.com/?url=" + queryURL;
     $.ajax({
       url: corsURL,
       method: "GET"
     }).then(function (response) {
       console.log(response);
-      
-        eventData = response;
-        
-        console.log("Data: ", eventData);
-        
+
+      eventData = response;
+
+      console.log("Data: ", eventData);
+
         for (var i = 0; i < 7; i++) {
         newEvent = "<tr>"
         newEvent += "<td>" + response.events[i].name.text + "</td>";
@@ -29,13 +29,14 @@ $(document).ready(function () {
         newEvent += "<td>" + response.events[i].venue.address.city + "</td>";
         newEvent += "<td>" + response.events[i].start.local + "</td>";
         newEvent += "</tr>";
+        
         $('.table').append(newEvent);
       };
     });
-    
+
   })
-  
+
 });
-  
+
 
 
